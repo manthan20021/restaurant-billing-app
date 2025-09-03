51f5fc5ece66331de360707226e6c1c21de514da
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2025-09-03 10:04:12 -0700

    made all routs protectes

--- routes/menu-route.js
+++ routes/menu-route.js
@@ -1,12 +1,12 @@
 import express from 'express'
 let route = express.Router();
 import { addMenu, deleteMenu, getMenu, updateMenu } from '../controllers/menuController.js';
-//import authMiddlerwer from '../middlewares/authMiddleware.js';
+import authMiddlerwer from '../middlewares/authMiddleware.js';
 
 
-route.post("/menu", addMenu);
-route.get("/menu",  getMenu);
-route.put("/menu/put/:id", updateMenu);
-route.delete("/menu/delete/:id", deleteMenu);
+route.post("/menu", authMiddlerwer, addMenu);
+route.get("/menu", authMiddlerwer,  getMenu);
+route.put("/menu/put/:id", authMiddlerwer, updateMenu);
+route.delete("/menu/delete/:id", authMiddlerwer, deleteMenu);
 
 export default route
